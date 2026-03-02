493eeda46edbe25b61415bba62a43bd7d6dec539
yimiro <s_icay@naver.com>
2026-03-02 19:45:06 +0900

    add get meeting spec

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.dtos.get_meeting_response import GetMeetingResponse
 from app.services.meeting_service_edgedb import service_create_meeting_edgedb
 from app.services.meeting_service_mysql import service_create_meeting_mysql
 
@@ -19,3 +20,13 @@ async def api_create_meeting_edgedb() -> CreateMeetingResponse:
 @mysql_router.post("", description="meeting을 생성하비다.")
 async def api_create_meeting_mysql() -> CreateMeetingResponse:
     return CreateMeetingResponse(url_code=(await service_create_meeting_mysql()).url_code)
+
+
+@edgedb_router.get("/{meeting_url_code}", description="meeting을 조회합니다.")
+async def api_get_meeting_edgedb(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
+
+
+@mysql_router.get("/{meeting_url_code}", description="meeting을 조회합니다.")
+async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
